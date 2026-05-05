6f2a90642131909707bc76a6038a7a35d2408aac
first-aicitizen <jhiel.tendero@fcpc.edu.ph>
2026-05-05 12:30:59 +0800

    Add files via upload

---
+++ func.js
@@ -0,0 +1,48 @@
+let hlCard = document.getElementById('hl_card');
+let aeCard = document.getElementById('ae_card');
+let rcCard = document.getElementById('rc_card');
+let ciCard = document.getElementById('ci_card');
+let saCard = document.getElementById('sa_card');
+let dtCard = document.getElementById('dt_card');
+let caCard = document.getElementById('ca_card');
+let programsCard = document.getElementById('programs_card');
+
+// History & Legacy Card
+hlCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=bf443c45-9eef-4934-9d60-de2ca0d340ae', '_blank');
+})
+
+// Academic Excellence Card
+aeCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=3606e28c-a8c0-44c5-b230-83b8c561854b', '_blank');
+})
+
+// Research & Collaboration Card
+rcCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=0cd832e2-c005-44c8-a898-2b2ce5fc868d', '_blank');
+})
+
+// Community Impact Card
+ciCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=0250cd4e-2f71-48f7-87ad-a4090e878c08', '_blank');
+})
+
+// Student Achievements Card
+saCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=5340d31d-5bc9-4a0b-9dc1-191ae3e9604d', '_blank');
+})
+
+// Digital Transformation Card
+dtCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=27881507-aca4-46bf-b74f-c448ea021088', '_blank');
+})
+
+// Culture & Arts Card
+caCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=51b48ba1-34c3-42f7-8936-45219f6a5d50', '_blank');
+})
+
+// Programs Card
+programsCard.addEventListener('click', function(){
+    window.open('https://app.capabara.ai/dashboard/chat?toolId=80df04e0-2301-480f-b23a-501be7a640c5', '_blank');
+})
